5e2bc3caf3407f6d59745162f12097345929dfb4
Amy Boudsady <aeimskei@gmail.com>
2018-05-08 20:39:27 -0700

    Organize tests with describe

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,39 +1,50 @@
 import data from '../../data/courses.json';
 
+// Organize test with describe
+
 // Numbers
 const numItems = data.length;
 
-test('Number of items = 12', () => {
+describe('Number tests', () => {
+  test('Number of items = 12', () => {
     expect(numItems).toBe(12);
-});
+  });
 
-test('Number of items to be greater than 12', () => {
+  test('Number of items to be greater than 12', () => {
     expect(numItems).toBeGreaterThanOrEqual(12);
-});
+  });
+})
 
 // Strings
 const dataTest = data[0].title;
 
-test('There is a JS in this title', () => {
-    expect(dataTest).toMatch(/JS/);
-});
+describe('String test', () => {
+	test('There is a JS in this title', () => {
+		expect(dataTest).toMatch(/JS/);
+	});
 
-test('The title contains React', () => {
-    expect(dataTest).toContain('React');
+	test('The title contains React', () => {
+		expect(dataTest).toContain('React');
+	});
 });
 
 // Arrays
 const data2 = ['React Native', 'MeteorJS'];
 
-test('The list of course mentions React native and MeteorJS', () => {
-    expect(['React Native', 'MeteorJS', 'React']).toEqual(expect.arrayContaining(data2));
-});
+describe('Array test', () => {
+	test('The list of course mentions React native and MeteorJS', () => {
+		expect(['React Native', 'MeteorJS', 'React']).toEqual(expect.arrayContaining(data2));
+	});
+})
 
 // Objects
-test('The first course to have a property of Title', () => {
-    expect(data[0]).toHaveProperty('title');
-});
 
-test('The first course to have a property of Title', () => {
-    expect(data[0]).toHaveProperty('views', '31,266');
+describe('Object test', () => {
+	test('The first course to have a property of Title', () => {
+		expect(data[0]).toHaveProperty('title');
+	});
+
+	test('The first course to have a property of Title', () => {
+		expect(data[0]).toHaveProperty('views', '31,266');
+	});
 });
