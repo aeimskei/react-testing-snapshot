80aa7af1ec58a91cc4da7e72aa027eaa1d4d3a51
Amy Boudsady <aeimskei@gmail.com>
2018-05-08 20:46:35 -0700

    Add code coverage report

---
+++ src/components/listings/Single.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Single from './Single';
+
+describe('Single sanpshot', () => {
+  test('Single snapshot test', () => {
+    const component = renderer.create(<Single />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
